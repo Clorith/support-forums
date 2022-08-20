1bdaabb9869fa5194b8eccf72c4443e34957633b
Marius Jensen <marius@clorith.net>
2022-08-20 14:16:05 +0200

    Convert to a WP-CLI setup process instead of a SQL file dump

--- bin/index.sh
+++ bin/index.sh
@@ -11,19 +11,6 @@ npm run wp-env start --update
 ###
 npm run wp-env run cli "wp core multisite-convert"
 
-# Create the plugins site.
-npm run wp-env run cli "wp site create --slug=plugins --title=Plugins"
-
-# Create the site.
-npm run wp-env run cli "wp site create --slug=themes --title=Themes"
-
-# WordPress, per now, does not properly identify port numbers in multisite URLs when creating
-# sites. Because of this, when we set it up using `wp-env`, followed by a `wp core multisite-convert`
-# from the WP-CLI package, strange things happen.
-# This search-replace is in place to undo the weirdness, and make the environment workable.
-npm run wp-env run cli "wp search-replace 'localhost:8888/:8888' 'localhost:8888' --skip-plugins --skip-themes --all-tables"
-npm run wp-env run cli "wp search-replace 'localhost8888' 'localhost:8888' --skip-plugins --skip-themes --all-tables"
-
 ###
 # Create the various users we will need
 ###
@@ -56,8 +43,10 @@ npm run wp-env run cli "wp user create visitor visitor@example.com --role=subscr
 # Set up the `plugins` sub-site with associated content.
 ###
 
+# Create the site.
+npm run wp-env run cli "wp site create --slug=plugins --title=Plugins"
+
 # Add site-specific plugins.
-npm run wp-env run cli "wp plugin activate jetpack --url=localhost:8888/plugins"
 npm run wp-env run cli "wp plugin activate plugin-directory --url=localhost:8888/plugins"
 
 # Add the plugin-related roles to this subsite.
@@ -66,11 +55,14 @@ npm run wp-env run cli "wp user set-role plugincontributor subscriber --url=loca
 npm run wp-env run cli "wp user set-role pluginsupport subscriber --url=localhost:8888/plugins"
 
 # Add `Hello Dolly` as a plugin.
-npm run wp-env run cli "wp post create --post_type=plugin --post_status=publish --post_author=4 --post_title='Hello Dolly' --post_date='2022-08-20 01:00:00' --post_modified='2022-08-20 01:00:00' --post_modified_gmt='2022-08-20 01:00:00' --url=localhost:8888/plugins"
+npm run wp-env run cli "wp post create --post_type=plugin --post_status=publish --post_author=4 --post_title='Hello Dolly' --url=localhost:8888/plugins"
 
 ###
 # Set up the `themes` sub-site with associated content.
 ###
+
+# Create the site.
+npm run wp-env run cli "wp site create --slug=themes --title=Themes"
 
 # Add site-specific plugins.
 npm run wp-env run cli "wp plugin activate theme-directory --url=localhost:8888/themes"
@@ -106,16 +98,9 @@ npm run wp-env run cli "wp user set-role themeauthor bbp_participant"
 npm run wp-env run cli "wp user set-role themesupport bbp_participant"
 npm run wp-env run cli "wp user set-role visitor bbp_participant"
 
-# Add the initial set of forums.
-npm run wp-env run cli "wp post create --post_type=forum --post_status=publish --post_title='Installing WordPress' --post_content='If you encounter any problems while setting up WordPress.' --post_date='2022-08-20 01:00:00' --post_modified='2022-08-20 01:00:00' --post_modified_gmt='2022-08-20 01:00:00'"
-npm run wp-env run cli "wp post create --post_type=forum --post_status=publish --post_title='Fixing WordPress' --post_content='For any problems encountered after setting up WordPress.' --post_date='2022-08-20 01:00:00' --post_modified='2022-08-20 01:00:00' --post_modified_gmt='2022-08-20 01:00:00'"
-npm run wp-env run cli "wp post create --post_type=forum --post_status=publish --post_title='Plugins' --post_content='Forum for plugin-specific support topics (hidden on WordPress.org).' --post_date='2022-08-20 01:00:00' --post_modified='2022-08-20 01:00:00' --post_modified_gmt='2022-08-20 01:00:00'"
-npm run wp-env run cli "wp post create --post_type=forum --post_status=publish --post_title='Themes' --post_content='Forum for theme-specific support topics (hidden on WordPress.org).' --post_date='2022-08-20 01:00:00' --post_modified='2022-08-20 01:00:00' --post_modified_gmt='2022-08-20 01:00:00'"
-npm run wp-env run cli "wp post create --post_type=forum --post_status=publish --post_title='Reviews' --post_content='Forum for plugin and theme reviews (hidden on WordPress.org).' --post_date='2022-08-20 01:00:00' --post_modified='2022-08-20 01:00:00' --post_modified_gmt='2022-08-20 01:00:00'"
-
-# Activate the forum theme.
-npm run wp-env run cli "wp theme activate wporg-support"
-
-# Setup, and flush rewrite rules.
-npm run wp-env run cli "wp rewrite structure '/%postname%/'"
-npm run wp-env run cli "wp rewrite flush"
+# Add the initial set of forums
+npm run wp-env run cli "wp post create --post_type=forum --post_status=publish --post_title='Installing WordPress' --post_content='If you encounter any problems while setting up WordPress.'"
+npm run wp-env run cli "wp post create --post_type=forum --post_status=publish --post_title='Fixing WordPress' --post_content='For any problems encountered after setting up WordPress.'"
+npm run wp-env run cli "wp post create --post_type=forum --post_status=publish --post_title='Plugins' --post_content='Forum for plugin-specific support topics (hidden on WordPress.org).'"
+npm run wp-env run cli "wp post create --post_type=forum --post_status=publish --post_title='Themes' --post_content='Forum for theme-specific support topics (hidden on WordPress.org).'"
+npm run wp-env run cli "wp post create --post_type=forum --post_status=publish --post_title='Reviews' --post_content='Forum for plugin and theme reviews (hidden on WordPress.org).'"
